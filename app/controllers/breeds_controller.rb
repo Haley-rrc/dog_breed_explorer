d4a513e6533d3d73a6f52bd9b3274966dfe053eb
HALEY.J <hjiang35@rrc.ca>
2026-07-10 02:27:24 -0500

    Add pages to breed list

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -8,6 +8,18 @@ class BreedsController < ApplicationController
         "%#{params[:query].downcase}%"
       )
     end
+
+    case params[:category]
+    when "with_sub_breeds"
+      @breeds = @breeds.joins(:sub_breeds).distinct
+    when "without_sub_breeds"
+      @breeds = @breeds.left_joins(:sub_breeds)
+                         .where(sub_breeds: { id: nil })
+    when "featured"
+      @breeds = @breeds.where(featured: true)
+    end
+
+    @breeds = @breeds.page(params[:page]).per(12)
   end
 
   def show
