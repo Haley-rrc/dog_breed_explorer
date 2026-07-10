9e97bdb2e655edbc905a756d7ef3ae2d4df0ec5b
HALEY.J <hjiang35@rrc.ca>
2026-07-10 02:39:03 -0500

    Add singke page for sub breeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "breeds#index"
 
   resources :breeds, only: [:index, :show]
+  resources :sub_breeds, only: [:show]
 
   get "about", to: "pages#about", as: :about
 end
