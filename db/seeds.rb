d927d3067c03f444b125e9459212707e3756ad87
HALEY.J <hjiang35@rrc.ca>
2026-07-10 01:49:04 -0500

    Add faker and update zseeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,68 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "net/http"
+require "json"
+require "faker"
+
+puts "Deleting old data..."
+
+DogImage.destroy_all
+SubBreed.destroy_all
+Breed.destroy_all
+
+puts "Downloading breed data from Dog CEO API..."
+
+breed_list_url = URI("https://dog.ceo/api/breeds/list/all")
+breed_response = Net::HTTP.get_response(breed_list_url)
+
+unless breed_response.is_a?(Net::HTTPSuccess)
+  puts "Could not download breed data."
+  exit
+end
+
+breed_data = JSON.parse(breed_response.body)
+
+unless breed_data["status"] == "success"
+  puts "The Dog CEO API returned an error."
+  exit
+end
+
+breed_data["message"].each do |breed_name, sub_breed_names|
+  breed = Breed.create!(
+    name: breed_name.titleize,
+    description: Faker::Lorem.sentence(word_count: 10),
+    featured: [true, false].sample
+  )
+
+  sub_breed_names.each do |sub_breed_name|
+    breed.sub_breeds.create!(
+      name: sub_breed_name.titleize
+    )
+  end
+
+  image_url = URI(
+    "https://dog.ceo/api/breed/#{breed_name}/images/random/3"
+  )
+
+  image_response = Net::HTTP.get_response(image_url)
+
+  next unless image_response.is_a?(Net::HTTPSuccess)
+
+  image_data = JSON.parse(image_response.body)
+
+  next unless image_data["status"] == "success"
+
+  Array(image_data["message"]).each_with_index do |url, index|
+    breed.dog_images.create!(
+      image_url: url,
+      caption: "#{breed.name} image #{index + 1}"
+    )
+  end
+
+  puts "Created #{breed.name}"
+end
+
+puts
+puts "Seed completed."
+puts "Breeds: #{Breed.count}"
+puts "Sub-breeds: #{SubBreed.count}"
+puts "Dog images: #{DogImage.count}"
+puts "Total rows: #{Breed.count + SubBreed.count + DogImage.count}"
