8cfce6758b2f3e1286661d5777e0bb410ec5f83a
HALEY.J <hjiang35@rrc.ca>
2026-07-10 02:13:51 -0500

    Add breed name search

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,6 +1,13 @@
 class BreedsController < ApplicationController
   def index
     @breeds = Breed.order(:name)
+
+    if params[:query].present?
+      @breeds = @breeds.where(
+        "LOWER(name) LIKE ?",
+        "%#{params[:query].downcase}%"
+      )
+    end
   end
 
   def show
